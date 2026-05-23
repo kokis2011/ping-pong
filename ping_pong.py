fd8efac5fe2784102ff5699c923253e4104dacbe
kokis2011 <viaziskonnos@gmail.com>
2026-05-23 16:42:08 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -1,4 +1,4 @@
-from pygame import*
+from pygame import *
 
 
 
@@ -11,63 +11,95 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
 #Player class
 class Player(GameSprite):
+
     def update_l(self):
         keys = key.get_pressed()
+
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 100:
+
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_l] and self.rect.y > 5:
+
+        if keys[K_i] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_k] and self.rect.y < win_height - 500:
+
+        if keys[K_k] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
-#window
+
+#Window setup
 background = (200, 255, 255)
+
 win_width = 600
 win_height = 500
+
 window = display.set_mode((win_width, win_height))
-display.set_caption("Ping-Pong game")
-window.fill(background)
-
-#racket1
-racket1 = Player('racket.png', 0, 100, 5, 100 , 150)
+display.set_caption("Ping-Pong Game")
 
 
+#Create player
+racket1 = Player('racket.png', 0, 100, 5, 50, 150)
+racket2 = Player('racket.png', 480, 100, 5, 50, 150)
+
+#ball
+ball = GameSprite('ball.png', 200, 200, 5, 50, 50)
 
 #FPS
 clock = time.Clock()
 FPS = 60
 
+#ball speed
+speed_x = 3
+speed_y = 3
 
-
-
-
-
-
-
-
-
-#game loop
+#Game loop
 game = True
 finish = False
 while game:
+
+    #Background refresh
+    window.fill(background)
+
+    #Events
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    #Auto ball movement
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-    display.update()
-    clock.tick(FPS)
+    if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+        speed_y *= -1
+    
+    #collisions with ball
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+
+    #Update
     racket1.update_l()
+    racket2.update_r()
+    ball.update()
 
-
+    #reset
     racket1.reset()
+    racket2.reset()
+    ball.reset()
+
+    #Update screen
+    display.update()
+
+    #FPS limit
+    clock.tick(FPS)
